5affc38b5a20ad97283d741b1df563029299d2b0
godfrey <gaobaoru2010@126.com>
2016-06-27 15:23:01 +0800

    Update remove-duplicates-from-sorted-array.cpp

--- remove-duplicates-from-sorted-array.cpp
+++ remove-duplicates-from-sorted-array.cpp
@@ -8,6 +8,7 @@ For example,
  Given input array A =[1,1,2], 
 Your function should return length =2, and A is now[1,2]. 
 
+时间复杂度O（n）,空间复杂度O（1）
 class Solution {
 public:
     int removeDuplicates(int A[], int n) {
