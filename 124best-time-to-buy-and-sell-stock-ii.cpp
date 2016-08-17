23daec3bd4b400846b4a5bb8668feeabe621c517
gaobaoru-lenovo <gaobaoru2010@126.com>
2016-08-17 11:10:30 +0800

    leetcode exercise best time to buy and sell stock

---
+++ 124best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,22 @@
+题目描述
+
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like 
+(ie, buy one and sell one share of the stock multiple times).
+ However, you may not engage in multiple transactions at the same time 
+(ie, you must sell the stock before you buy again). 
+
+class Solution {
+public:
+    //时间复杂度Ｏ(n),空间复杂度Ｏ(1)
+    int maxProfit(vector<int> &prices) {
+        int profit = 0;
+        for(int i = 1; i < prices.size(); i++){
+            int diff = prices[i] - prices[i-1];
+            if(diff > 0) profit += diff;
+        }
+        return profit;
+    }
+};
